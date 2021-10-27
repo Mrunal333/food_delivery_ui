e464bedb73e80b901218fee265a392f637d630bd
Mrunal333 <mrunalsheramkar333@2gmail.com>
2021-10-27 17:07:22 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// @dart=2.7
+import 'package:flutter/material.dart';
+import 'package:food_delivery_ui/pages/StarterPage.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Roboto'),
+      home: StarterPage(),
+    ));
+}
+
+
+
